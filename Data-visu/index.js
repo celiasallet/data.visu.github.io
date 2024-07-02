e5cc1437c944c35f631b4762c2183512571f0269
celiasallet <celiasallet@yahoo.fr>
2024-07-02 16:53:03 +0200

    grabbing data

--- Data-visu/index.js
+++ Data-visu/index.js
@@ -1,7 +1,16 @@
 var tableId = document.getElementById('table1');
-var tBody = tableId.getElementsByTagName('tbody')[0];
-var tableRow = tBody.getElementsByTagName('tr');
+var tBody = document.getElementsByTagName('tbody');
+var tableRow = document.getElementsByTagName('tr');
 
-for (var t = 0; t < tableRow.length; t++){
-    console.log(tableRow[t].innerHTML)
+for (var t = 0; t < tableRow.length; t++) {
+  var cells = tableRow[t].getElementsByTagName('td');
+  var rowText = '';
+  for (var c = 0; c < cells.length; c++) {
+    rowText += cells[c].textContent.trim() + ' '; 
+  }
+  var headers = tableRow[t].getElementsByTagName('th');
+  for (var h = 0; h < headers.length; h++) {
+    rowText += headers[h].textContent.trim() + ' '; 
+  }
+  console.log(rowText.trim()); 
 }
